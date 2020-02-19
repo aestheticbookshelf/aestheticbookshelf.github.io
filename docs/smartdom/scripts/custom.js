eb84941a46e905adb6c1d8f97663e33910f0feaf
aestheticbookshelf <aestheticbookshelf@gmail.com>
2020-02-19 06:10:28 +0100

    Smartdom docs

--- docs/smartdom/scripts/custom.js
+++ docs/smartdom/scripts/custom.js
@@ -15,3 +15,20 @@ class OptParam extends HTMLElement{
 
 
 customElements.define('opt-param', OptParam)
+
+class PropsOpt extends HTMLElement{
+    constructor(){
+        super()
+        let shadow = this.attachShadow({mode: 'open'});
+
+        let div = document.createElement("div")
+        div.style.display = "inline-block"        
+        div.style.padding = "2px"
+        div.style.fontStyle = "italic"
+        div.innerHTML = "props when defined is a dictionary with optional members"
+
+        shadow.appendChild(div)
+    }
+}
+
+customElements.define('props-opt', PropsOpt)
